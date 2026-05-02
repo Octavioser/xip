e77747f98bd765611e3b84f63aa9d5df43ff9634
Triton <limhunsuck@naver.com>
2026-05-02 22:32:48 +0900

    음악 자동재생 제거 및 동영상 수정

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,17 +1,13 @@
-"use client";
-
 import { LoadingLink } from "@/components/common/LoadingLink";
 import { PBtn } from "@/components/common/PBtn";
-import { useAppContext } from "@/contexts/AppContext";
 import styles from "./page.module.scss";
 
 export default function StartPage() {
-  const { setMusicPlaying } = useAppContext();
   return (
     <div className={`beforeStart ${styles.bg}`}>
       <div className={styles.center}>
         <div className={styles.entryBox}>
-          <LoadingLink href="/home" onClick={() => setMusicPlaying(true)}>
+          <LoadingLink href="/home">
             <PBtn labelText="click to enter" className={styles.entryText} />
           </LoadingLink>
         </div>
